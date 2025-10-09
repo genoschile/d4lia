0a211e7f2e1f6e827c5f27256fdf2249f2f78216
Benjamín Ignacio Astudillo <bastudillo.alarcon@gmail.com>
2025-10-09 03:50:25 +0200

    up

--- app/main.py
+++ app/main.py
@@ -13,3 +13,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.on_event("startup")
+async def startup():
+    await connect_to_db()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await close_db_connection()
