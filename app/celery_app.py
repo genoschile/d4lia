7dfa42b03f6bd4322b8173abeb5a67ad0fb5beab
benjamin <benja_e_e@hotmail.com>
2025-10-31 12:49:15 -0300

    up

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,10 +1,10 @@
+import os
 from celery import Celery
 
-celery_app = Celery(
-    "app",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/1"
-)
+BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+BACKEND_URL = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
+
+celery_app = Celery("app", broker=BROKER_URL, backend=BACKEND_URL)
 
 celery_app.conf.update(
     task_serializer="json",
